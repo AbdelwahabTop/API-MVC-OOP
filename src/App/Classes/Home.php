6575715ba72182e59b4b201ccd661e92d746f48b
Abdelwahab <mramged936@gmail.com>
2023-06-08 18:10:41 +0300

    SESSIONS COOKIES FILES

--- src/App/Classes/Home.php
+++ src/App/Classes/Home.php
@@ -6,8 +6,29 @@ namespace App\Classes;
 
 class Home
 {
-    public function index()
+    public function index(): string
     {
-        return 'Home';
+        return <<<FORM
+        <form action="/upload" method="post" enctype="multipart/form-data">
+            <input name="receipt" type="file">
+            <input name="myimage" type="file">
+            <button type="submit">Upload</button>
+        </form>
+        FORM;
+    }
+
+    public function upload()
+    {
+        echo '<pre>';
+        var_dump($_FILES);
+        echo '</pre>';
+
+        $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
+
+        move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
+
+        echo '<pre>';
+        var_dump(pathinfo($filePath));
+        echo '</pre>';
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,16 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+session_start();
+
+define('STORAGE_PATH', __DIR__ . '/../storage');
 
 $router = new App\Router();
 
 $router
     ->get('/', [App\Classes\Home::class, 'index'])
+    ->post('/upload', [App\Classes\Home::class, 'upload'])
     ->get('/invoices', [App\Classes\Invoice::class, 'index'])
     ->get('/invoices/create', [App\Classes\Invoice::class, 'create'])
     ->post('/invoices/create', [App\Classes\Invoice::class, 'store']);
