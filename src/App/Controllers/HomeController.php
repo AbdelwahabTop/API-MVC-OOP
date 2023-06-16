4ca634e4d9a2530e44a349601b16a66a899af494
Abdelwahab <mramged936@gmail.com>
2023-06-16 12:15:39 +0300

    PDO and Default fetch mode

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use PDO;
 
 class HomeController
 {
     public function index(): View
     {
+        try {
+            $db = new PDO('mysql:host=localhost;dbname=my_db', 'root', '', [
+                // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+            ]);
+
+            $query = 'SELECT * FROM users';
+
+            $stmt = $db->query($query);
+
+            dump($stmt->fetchAll());
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), $e->getCode());
+        }
+
+        var_dump($db);
+
         return View::make('index', ['foo' => 'bar']);
     }
 
