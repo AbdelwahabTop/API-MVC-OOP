b0122c1f2e8fe996b82ed4a681ec331c5380ac46
Abdelwahab <mramged936@gmail.com>
2023-06-16 17:43:10 +0300

    INSERT INTO

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -12,22 +12,27 @@ class HomeController
     public function index(): View
     {
         try {
-            $db = new PDO('mysql:host=localhost;dbname=my_db', 'root', '', [
-                // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-            ]);
-
-            $email = $_GET['email'];
-            $query = 'SELECT * FROM users WHERE email = ?';
-
-            $stmt = $db->prepare($query);
-
-            $stmt->execute([$email]);
-
-            dump($stmt->fetchAll());
-
+            $db = new PDO('mysql:host=localhost;dbname=my_db', 'root', '');
         } catch (\PDOException $e) {
             throw new \PDOException($e->getMessage(), $e->getCode());
         }
+
+        $email = 'abdo@gmail.com';
+        $name = 'abdo';
+        $isActive = 1;
+        $createdAt = date('Y-m-d H:m:i', strtotime('07/11/2021 9:00PM'));
+        $query = 'INSERT INTO users (email, full_name, is_active, create_at)
+                VALUES (?, ?, ?, ?)';
+
+        $stmt = $db->prepare($query);
+
+        $stmt->execute([$email, $name, $isActive, $createdAt]);
+
+        $id = (int) $db->lastInsertId();
+
+        $user = $db->query('SELECT * FROM users WHERE id = ' . $id);
+
+        dump($stmt->fetchAll());
 
         var_dump($db);
 
